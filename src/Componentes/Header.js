5e2b8e7e16af99da296823f685237e6b20d43506
caiioalves <godofredo.b123@gmail.com>
2022-12-22 20:30:05 -0300

    Corrigindo erros de responsividade

--- src/Componentes/Header.js
+++ src/Componentes/Header.js
@@ -1,52 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { AppBar, Box, Typography } from '@mui/material';
+import { AppBar, Box, Stack } from '@mui/material';
 
 class Header extends React.Component {
   render() {
     const { email, despesas } = this.props;
     return (
-      <AppBar
-        position="static"
-        sx={{
-          // marginLeft: -13,
-          width: "100%",
-          height: "10%",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <Typography
-          // variant='h5'
-          data-testid="email-field"
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            marginLeft: 1
-          }}
-        >
-          { email }
-        </Typography>
-        <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-around",
-          alignItems: "center",
-          minWidth: "12%",
-          maxWidth: "12%"
-        }}>
-        <Typography data-testid="total-field">
+      <AppBar position="static">
+        <Stack p={2} direction="row" spacing={2} justifyContent="space-between">
+        <Box>{ email }</Box>
+        <Box>
           {
             despesas.reduce((prevValor, valor) => (
               prevValor + valor.value * Number(valor.exchangeRates[valor.currency].ask)
             ), 0).toFixed(2)
           }
-        </Typography>
-        <Typography data-testid="header-currency-field">BRL</Typography>
+          BRL
         </Box>
+        </Stack>
       </AppBar>
     );
   }
@@ -61,7 +33,6 @@ const mapStateToProps = (state) => ({
 Header.propTypes = {
   email: PropTypes.string.isRequired,
   despesas: PropTypes.arrayOf.isRequired,
-// valorTotal: PropTypes.number.isRequired,
 };
 
 export default connect(mapStateToProps)(Header);
