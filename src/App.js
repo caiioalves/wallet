2a3c07ceded91c19174a1bef5d178c66604d73f7
caiioalves <godofredo.b123@gmail.com>
2023-01-23 23:14:00 -0300

    Removendo arquivos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
-import Register from './pages/Register';
 import Wallet from './pages/Wallet';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/" component={ Login } />
-          <Route exact path="/register" component={ Register } />
           <Route path="/carteira" component={ Wallet } />
         </Switch>
       </div>
